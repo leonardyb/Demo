58e8f494119d81dbc375c958f04341087d56b7c8
Your Name <you@example.com>
2019-06-11 23:00:38 +0800

    更新Demo項目添加了StudentTest.java测试类

---
+++ src/com/lyb/java/StudentTest.java
@@ -0,0 +1,24 @@
+package com.lyb.java;
+
+/**
+ * @author liuyabo
+ * @create 2019-06-11 22:11
+ */
+public class StudentTest {
+
+    public static void main(String[] args) {
+
+        Student student = new Student();
+        student.setCardId("610324199409214031");
+        student.setName("liuyabo");
+        student.setAge(25);
+        student.setPhone("15001968297");
+
+        StudentTest.show(student);
+    }
+
+    public static void show(Student student) {
+
+        System.out.println("省份证：" + student.getCardId());
+    }
+}
